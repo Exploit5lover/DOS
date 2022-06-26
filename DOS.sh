842c8a3af0da482325125433d9c2c550121d55e5
Exploit5lover <107794615+Exploit5lover@users.noreply.github.com>
2022-06-26 15:51:11 +0500

    Update DOS.sh

--- DOS.sh
+++ DOS.sh
@@ -1,3 +1,21 @@
+echo -ne '>>>                       [20%]\r'
+# some task
+sleep 2
+echo -ne '>>>>>>>                   [40%]\r'
+# some task
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [60%]\r'
+# some task
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
+# some task
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -ne '\n'
+
+
+
 figlet Sameer
-Figlet khan
-Figlet hacker
+figlet khan
+figlet hacker
+
